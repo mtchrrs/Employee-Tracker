1a70c9d9c0577463fd1d0746f0e227e9f6747b16
Money Maker <mitchell.harris@westnet.com.au>
2022-11-04 21:37:52 +0800

    Added the index.js file, added seeds.sql, will need to test and edit the existing file structure still

--- db/schema.sql
+++ db/schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE roles (
   FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
