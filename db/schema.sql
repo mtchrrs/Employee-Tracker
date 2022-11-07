e1ae96b01f1a2704983e5f7195a1f8bd41d5de4c
Money Maker <mitchell.harris@westnet.com.au>
2022-11-07 20:50:51 +0800

    troubleshooting

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,9 @@
 DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 
-USE employee_db;
+USE company_db;
 
-CREATE TABLE department (
+CREATE TABLE departments (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
@@ -13,7 +13,7 @@ CREATE TABLE roles (
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT,
-  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+  FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
